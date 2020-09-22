09d0c0ad836850350a9117aa898080b564823d0b
Haseeb Zia <haseebzia99@outlook.com>
2020-09-22 21:55:09 +0100

    search and index redone

--- app/controllers/tvs_controller.rb
+++ app/controllers/tvs_controller.rb
@@ -1,2 +1,17 @@
 class TvsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @tvs = Tv.all
+    if !params[:query].nil?
+      @tvs = Tv.where('name ILIKE ?', params[:query])
+    end
+    if !params[:search].nil?
+      params[:search].each do |key, value|
+        @tvs = @tvs.where(key => value) if value.present?
+      end
+    end
+  end
+
+
 end
--- app/models/tv.rb
+++ app/models/tv.rb
@@ -1,4 +1,11 @@
 class Tv < ApplicationRecord
   has_many :transactions
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_year,
+    against: [ :name, :year],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
 
