a717a6ce11e6633f8b0c9c4a2366b5f0859bf5fd
Haseeb Zia <haseebzia99@outlook.com>
2020-10-06 17:34:08 +0100

    checkbox and dropdown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to user_path(current_user)
+  end
 end
