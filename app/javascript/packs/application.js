f50c6e7699a7b0aeffb122809e92311bf615cefa
Haseeb Zia <haseebzia99@outlook.com>
2020-10-17 20:31:09 +0100

    navbar complete with placeholder as well as hamburger js option

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,3 +37,15 @@ document.addEventListener('turbolinks:load', () => {
 $ (function () {
   AOS.init ();
 });
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
